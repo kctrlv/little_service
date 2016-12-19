0f481262a1a81d5c3624f2511e85083c21fa9638
David Davydov <kctrlv@gmail.com>
2016-12-19 09:55:56 -0700

    add byebug

--- app/controllers/api/v1/commit_controller.rb
+++ app/controllers/api/v1/commit_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CommitController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    require "pry"; binding.pry
+    byebug
 
     # Commit.create(commit_params)
   end
