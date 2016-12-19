d74fa41c383df5fc092e20d9e2d54a09dff94348
David Davydov <kctrlv@gmail.com>
2016-12-19 09:54:04 -0700

    commit params

--- app/controllers/api/v1/commit_controller.rb
+++ app/controllers/api/v1/commit_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::CommitController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  
+
   def create
-    puts params
+    Commit.create(commit_params)
+  end
+
+  private
+
+  def commit_params
+    require "pry"; binding.pry
   end
 end
