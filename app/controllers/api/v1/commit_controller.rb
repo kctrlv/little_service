17f037f5ab801ac07979ea8d42bc7d4053354f9f
David Davydov <kctrlv@gmail.com>
2016-12-19 09:54:47 -0700

    pry in create action

--- app/controllers/api/v1/commit_controller.rb
+++ app/controllers/api/v1/commit_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::CommitController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    Commit.create(commit_params)
+    require "pry"; binding.pry
+
+    # Commit.create(commit_params)
   end
 
   private
